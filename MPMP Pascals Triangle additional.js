55e8cccc54e3f1196a4bdb85403843bc786ce17f
Fredemb <fredemb@gmail.com>
2020-10-16 09:43:50 +0200

    Changed the generation of the triangle numbers to bools
    To follow along, and view percentages, use of the developer console is still required.

--- MPMP Pascals Triangle additional.js
+++ MPMP Pascals Triangle additional.js
@@ -5,10 +5,10 @@ var odd = 0
 var rowtotal = 0
 var rowodd = 0
 
-function checkOdd(n) {
+function checkOdd(bool) { //Odd numbers are represented by boolean value TRUE, even numbers are FALSE
     total += 1
     rowtotal += 1
-    if (!(n % 2n == 0n)) {
+    if (bool) {
         odd += 1
         rowodd += 1
     }
@@ -16,7 +16,7 @@ function checkOdd(n) {
 
 function generateTriangle(numberOfRows) {
     var triangle = [] //Variable to store pascals triangle
-    triangle.push([BigInt(1)]) //Starting the first row as a single 1
+    triangle.push([true]) //Starting the first row as a single odd number
     triangle[0].forEach(checkOdd) //Count the odd numbers in the first row
     
     for (i=1; i<numberOfRows; i++){ //Loop to generate pascals triangle
@@ -26,18 +26,19 @@ function generateTriangle(numberOfRows) {
         rowtotal = 0
         rowodd = 0
     
-        nextRow.push(lastRow[0]) //Add the first number of the last row as the first number of this row
+        nextRow.push(lastRow[0]) //Add the first value of the last row as the first number of this row
         checkOdd(lastRow[0])
     
         if (lastRow.length > 1) {
-            for (j=0; j<lastRow.length-1; j++){ //This loop does the summing for all numbers except the first and last of any row
-                var sum = BigInt(lastRow[j] + lastRow[j+1])
-                nextRow.push(BigInt(sum))
-                checkOdd(BigInt(sum))
+            for (j=0; j<lastRow.length-1; j++){ //This loop calculates wheather summed numbers are true or false, and adds them to the row
+                //var sum = BigInt(lastRow[j] + lastRow[j+1])
+                var sum = ( (lastRow[j] || lastRow[j+1]) && !(lastRow[j] && lastRow[j+1]) ) //Curse the lack of an XOR operator ;-P
+                nextRow.push(sum)
+                checkOdd(sum)
             }
         }
     
-        nextRow.push(lastRow[lastRow.length-1]) //Add the last number of the last row as the last number of this row
+        nextRow.push(lastRow[lastRow.length-1]) //Add the last value of the last row as the last number of this row
         checkOdd(lastRow[lastRow.length-1])
     
         triangle.push(nextRow)
