d6885cc6057a49e1aa1f2022594596e2691a32ea
Fredemb <fredemb@gmail.com>
2020-10-16 11:15:29 +0200

    Added UI output

--- MPMP Pascals Triangle additional.js
+++ MPMP Pascals Triangle additional.js
@@ -1,14 +1,38 @@
-document.getElementById("rows").value = 128
+document.getElementById("rows").value = 128 //Standard number of rows set to 128, as in the original puzzle
 
+//Add variables to hold counters
 var total = 0
 var odd = 0
 var rowtotal = 0
 var rowodd = 0
 
+//Add variables to hold ratios
+var rowratio = (rowodd/rowtotal)*100
+var ratio = (odd/total)*100
+
+//Add references to UI elements
+let totalRowsUI = document.getElementById("rowsCalculated")
+let rowratioUI = document.getElementById("oddThisRow")
+let ratioUI = document.getElementById("oddAllRows")
+let calculateButton = document.getElementById("calculateButton")
+let inputnumber = document.getElementById("rows").value
+
+function updateUI(i) { //Function to update the output on screen
+
+    //Calculate new ratios
+    rowratio = (rowodd/rowtotal)*100
+    ratio = (odd/total)*100
+
+    //Print output to relevant UI elements
+    totalRowsUI.innerHTML = i
+    rowratioUI.innerHTML = rowratio.toFixed(4) + "%"
+    ratioUI.innerHTML = ratio.toFixed(4) + "%"
+}
+
 function checkOdd(bool) { //Odd numbers are represented by boolean value TRUE, even numbers are FALSE
     total += 1
     rowtotal += 1
-    if (bool) {
+    if (bool) { //Runs if the number is odd
         odd += 1
         rowodd += 1
     }
@@ -23,29 +47,29 @@ function generateTriangle(numberOfRows) {
         var lastRow = triangle[triangle.length-1]
         var nextRow = []
     
+        //Resetting the row counters
         rowtotal = 0
         rowodd = 0
     
         nextRow.push(lastRow[0]) //Add the first value of the last row as the first number of this row
-        checkOdd(lastRow[0])
+        checkOdd(lastRow[0]) //Count if odd, and count in total
     
         if (lastRow.length > 1) {
             for (j=0; j<lastRow.length-1; j++){ //This loop calculates wheather summed numbers are true or false, and adds them to the row
                 //var sum = BigInt(lastRow[j] + lastRow[j+1])
                 var sum = ( (lastRow[j] || lastRow[j+1]) && !(lastRow[j] && lastRow[j+1]) ) //Curse the lack of an XOR operator ;-P
-                nextRow.push(sum)
-                checkOdd(sum)
+                nextRow.push(sum) //Adds the sum to the current row
+                checkOdd(sum) //Count if odd, and count in total
             }
         }
     
         nextRow.push(lastRow[lastRow.length-1]) //Add the last value of the last row as the last number of this row
-        checkOdd(lastRow[lastRow.length-1])
+        checkOdd(lastRow[lastRow.length-1]) //Count if odd, and count in total
     
-        triangle.push(nextRow)
-        triangle.shift()
-    
-        var rowratio = (rowodd/rowtotal)*100
-        var ratio = (odd/total)*100
+        triangle.push(nextRow) //Adding the generated row to the triangle in memory
+        triangle.shift() //Removes previous rows from memory, for memory savings
+
+        updateUI(i) //I cant seem to get this to update before the loop is over :-(
     
         console.log(i + ": " + rowratio.toFixed(4) + "%  ->  " + ratio.toFixed(4) + "%")
     }
@@ -53,21 +77,22 @@ function generateTriangle(numberOfRows) {
     return triangle
 }
 
-let calculateButton = document.getElementById("calculateButton")
-var inputnumber = document.getElementById("rows").value
-
 function calculate() {
+
+    //First resets all counters, to ensure starting from 0
     total = 0
     odd = 0
     rowtotal = 0
     rowodd = 0
-    inputnumber = Number(document.getElementById("rows").value)
-    document.getElementById("output").innerHTML = null
-    generateTriangle(inputnumber).forEach(appendline)
+
+    inputnumber = Number(document.getElementById("rows").value) //Gets the number of rows to calculate from the user input
+    document.getElementById("output").innerHTML = null //Removes any previous output
+    updateUI(0) //Removes previous output
+    generateTriangle(inputnumber).forEach(appendline) //Generates the triangle, and then starts the appendline function (below)
 
     function appendline(line){
-        document.getElementById("output").innerHTML += line + "<br>"
+        document.getElementById("output").innerHTML += line + "<br>" //Writes the triangle from memory to screen - most of the triangle is dropped, so only the last line will appear
     }
 }
 
-calculateButton.addEventListener("click",calculate)
+calculateButton.addEventListener("click",calculate) //Listener for button-press 
--- README.md
+++ README.md
@@ -5,7 +5,6 @@ This project was made as an attempt to answer Matt Parkers Maths puzzle about Pa
 It works reasonably well for the first around 1000 rows of the triangle, but the further you go, the slower it becomes, and it will eventually crash due to lack of memory availible to store the triangle.
 I have tried to mitigate the memory problem by dropping every line as I no longer need it (as each line is generated from the last line, I only need to keep a line in memory untill I am finished calculating the next line)
 
-Since making this, I have realized that a more elegant solution, pushing back the memory problem even further, would have been to generalise the problem, by only storing a bool (eg true if the number is odd), 
-since I would still be able to accurately determine if two added numbers will yeild an odd or even number, without knowing the actual number - this would probably save me a lot of memory, and computation time.
+In this branch, I also don't keep the numbers, but only save a bool, that answers the question "is this number odd?" - to calculate if the sum of two numbers is odd, a simple XOR (which does not exist in javascript) will suffice - two odds or two evens will always sum to an even number, only one of each will sum to an odd number, no matter what the numbers actually are
 
 Also, I intuitively feel like there should be some mathmatically elegant solution, that doesn't rely on actually calculating tens of thousands of rows to arrive at an answer, but coding was just what first came to my mind.
